f52dc6ee708d88538962922ced38bbd069c1794c
ronaldo.silva7 <ronaldo.silva7@12SPLFS637718.seap.intranet>
2018-03-27 16:17:41 -0300

    Acrescentado atributos Fonte, Tamanho, pBtEsq, pBtCon, pBtDir, pBtJus

--- src/gestor/Modelo/OcorrenciasPortaria.java
+++ src/gestor/Modelo/OcorrenciasPortaria.java
@@ -24,8 +24,14 @@ public class OcorrenciasPortaria {
     private String dataUp;
     private String horarioInsert;
     private String horarioUp;
+    private String fonte;
+    private String Tamanho;
+    private int Btesq;
+    private int BtCen;
+    private int BtDir;
+    private int BtJus;
 
-    public OcorrenciasPortaria(Integer idLanc, String statusLanc, Date dataLanc, String titulo, String textoArea, String usuarioInsert, String usuarioUp, String dataInsert, String dataUp, String horarioInsert, String horarioUp) {
+    public OcorrenciasPortaria(Integer idLanc, String statusLanc, Date dataLanc, String titulo, String textoArea, String usuarioInsert, String usuarioUp, String dataInsert, String dataUp, String horarioInsert, String horarioUp, String fonte, String Tamanho, int Btesq, int BtCen, int BtDir, int BtJus) {
         this.idLanc = idLanc;
         this.statusLanc = statusLanc;
         this.dataLanc = dataLanc;
@@ -37,6 +43,12 @@ public class OcorrenciasPortaria {
         this.dataUp = dataUp;
         this.horarioInsert = horarioInsert;
         this.horarioUp = horarioUp;
+        this.fonte = fonte;
+        this.Tamanho = Tamanho;
+        this.Btesq = Btesq;
+        this.BtCen = BtCen;
+        this.BtDir = BtDir;
+        this.BtJus = BtJus;
     }
 
     public OcorrenciasPortaria() {
@@ -194,5 +206,89 @@ public class OcorrenciasPortaria {
      */
     public void setHorarioUp(String horarioUp) {
         this.horarioUp = horarioUp;
+    }
+
+    /**
+     * @return the fonte
+     */
+    public String getFonte() {
+        return fonte;
+    }
+
+    /**
+     * @param fonte the fonte to set
+     */
+    public void setFonte(String fonte) {
+        this.fonte = fonte;
+    }
+
+    /**
+     * @return the Tamanho
+     */
+    public String getTamanho() {
+        return Tamanho;
+    }
+
+    /**
+     * @param Tamanho the Tamanho to set
+     */
+    public void setTamanho(String Tamanho) {
+        this.Tamanho = Tamanho;
+    }
+
+    /**
+     * @return the Btesq
+     */
+    public int getBtesq() {
+        return Btesq;
+    }
+
+    /**
+     * @param Btesq the Btesq to set
+     */
+    public void setBtesq(int Btesq) {
+        this.Btesq = Btesq;
+    }
+
+    /**
+     * @return the BtCen
+     */
+    public int getBtCen() {
+        return BtCen;
+    }
+
+    /**
+     * @param BtCen the BtCen to set
+     */
+    public void setBtCen(int BtCen) {
+        this.BtCen = BtCen;
+    }
+
+    /**
+     * @return the BtDir
+     */
+    public int getBtDir() {
+        return BtDir;
+    }
+
+    /**
+     * @param BtDir the BtDir to set
+     */
+    public void setBtDir(int BtDir) {
+        this.BtDir = BtDir;
+    }
+
+    /**
+     * @return the BtJus
+     */
+    public int getBtJus() {
+        return BtJus;
+    }
+
+    /**
+     * @param BtJus the BtJus to set
+     */
+    public void setBtJus(int BtJus) {
+        this.BtJus = BtJus;
     }
 }
