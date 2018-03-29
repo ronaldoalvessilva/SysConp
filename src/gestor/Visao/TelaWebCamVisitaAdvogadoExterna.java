e2ae2bac61fa93fd1ad280ada0059e0deda1973b
ronaldo.silva7 <ronaldo.silva7@12SPLFS637718.seap.intranet>
2018-03-29 15:43:21 -0300

    Consertado o botão abrir, para abrir e salvar a foto tirada

--- src/gestor/Visao/TelaWebCamVisitaAdvogadoExterna.java
+++ src/gestor/Visao/TelaWebCamVisitaAdvogadoExterna.java
@@ -281,7 +281,7 @@ public class TelaWebCamVisitaAdvogadoExterna extends javax.swing.JDialog {
             seletor.setAcceptAllFileFilterUsed(false);
             seletor.setMultiSelectionEnabled(false);
             seletor.setFileFilter(tipoExtensao);
-            int acao = seletor.showOpenDialog(this);
+            int acao = seletor.showSaveDialog(this);
             if (acao == JFileChooser.APPROVE_OPTION) {
                 java.io.File f = new File(seletor.getSelectedFile().getAbsolutePath());
                 caminhoAdvogadoEXT = f.getPath();
@@ -304,7 +304,7 @@ public class TelaWebCamVisitaAdvogadoExterna extends javax.swing.JDialog {
             seletor.setAcceptAllFileFilterUsed(false);
             seletor.setMultiSelectionEnabled(false);
             seletor.setFileFilter(tipoExtensao);
-            int acao = seletor.showOpenDialog(this);
+            int acao = seletor.showSaveDialog(this);
             if (acao == JFileChooser.APPROVE_OPTION) {
                 java.io.File f = new File(seletor.getSelectedFile().getAbsolutePath());
                 caminhoAdvogadoEXT = f.getPath();
