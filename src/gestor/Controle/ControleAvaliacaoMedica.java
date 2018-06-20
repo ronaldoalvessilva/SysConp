be690ca142cafac52156a83a285e97d7553fd7e8
ronaldo.silva7 <ronaldo.silva7@12SPLFS637718.seap.intranet>
2018-06-19 23:57:32 -0300

    IMPLEMENTADO A FORMATAÇÃO DO TEXTO DA AVALIAÇÃO

--- src/gestor/Controle/ControleAvaliacaoMedica.java
+++ src/gestor/Controle/ControleAvaliacaoMedica.java
@@ -26,7 +26,7 @@ public class ControleAvaliacaoMedica {
         buscarInterno(objAvalia.getNomeInterno(),objAvalia.getIdInternoCrc());
         conecta.abrirConexao();
         try {
-            PreparedStatement pst = conecta.con.prepareStatement("INSERT INTO AVALIACAO_MEDICA_PSIQUIATRICA (StatusAval,DataAval,IdInternoCrc,TextoAvaliacao,UsuarioInsert,DataInsert,HorarioInsert) VALUES (?,?,?,?,?,?,?)");
+            PreparedStatement pst = conecta.con.prepareStatement("INSERT INTO AVALIACAO_MEDICA_PSIQUIATRICA (StatusAval,DataAval,IdInternoCrc,TextoAvaliacao,UsuarioInsert,DataInsert,HorarioInsert,Fonte,Tamanho,BtEsq,BtCen,BtDir,BtJus) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)");
             pst.setString(1, objAvalia.getStatusAval());
             pst.setTimestamp(2, new java.sql.Timestamp(objAvalia.getDataAval().getTime()));
             pst.setInt(3, codInterno);
@@ -34,6 +34,12 @@ public class ControleAvaliacaoMedica {
             pst.setString(5, objAvalia.getUsuarioInsert());
             pst.setString(6, objAvalia.getDataInsert());
             pst.setString(7, objAvalia.getHorarioInsert());
+            pst.setString(8, objAvalia.getFonte());
+            pst.setString(9, objAvalia.getTamanho());
+            pst.setInt(10, objAvalia.getBtesq());
+            pst.setInt(11, objAvalia.getBtCen());
+            pst.setInt(12, objAvalia.getBtDir());
+            pst.setInt(13, objAvalia.getBtJus());
             pst.execute();
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Não Foi possivel INSERIR os Dados.\n\nERRO: " + ex);
@@ -46,7 +52,7 @@ public class ControleAvaliacaoMedica {
         buscarInterno(objAvalia.getNomeInterno(),objAvalia.getIdInternoCrc());
         conecta.abrirConexao();
         try {
-            PreparedStatement pst = conecta.con.prepareStatement("UPDATE AVALIACAO_MEDICA_PSIQUIATRICA SET StatusAval=?,DataAval=?,IdInternoCrc=?,TextoAvaliacao=?,UsuarioUp=?,DataUp=?,HorarioUp=? WHERE IdAvalia='" + objAvalia.getIdAvalia() + "'");
+            PreparedStatement pst = conecta.con.prepareStatement("UPDATE AVALIACAO_MEDICA_PSIQUIATRICA SET StatusAval=?,DataAval=?,IdInternoCrc=?,TextoAvaliacao=?,UsuarioUp=?,DataUp=?,HorarioUp=?,Fonte=?,Tamanho=?,BtEsq=?,BtCen=?,BtDir=?,BtJus=? WHERE IdAvalia='" + objAvalia.getIdAvalia() + "'");
             pst.setString(1, objAvalia.getStatusAval());
             pst.setTimestamp(2, new java.sql.Timestamp(objAvalia.getDataAval().getTime()));
             pst.setInt(3, codInterno);
@@ -54,6 +60,12 @@ public class ControleAvaliacaoMedica {
             pst.setString(5, objAvalia.getUsuarioUp());
             pst.setString(6, objAvalia.getDataUp());
             pst.setString(7, objAvalia.getHorarioUp());
+            pst.setString(8, objAvalia.getFonte());
+            pst.setString(9, objAvalia.getTamanho());
+            pst.setInt(10, objAvalia.getBtesq());
+            pst.setInt(11, objAvalia.getBtCen());
+            pst.setInt(12, objAvalia.getBtDir());
+            pst.setInt(13, objAvalia.getBtJus());
             pst.executeUpdate();
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Não Foi possivel ALTERAR os Dados.\n\nERRO: " + ex);
