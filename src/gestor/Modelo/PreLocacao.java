5369d3f77bcc043d6f800ad73fb61ecf941084cb
ronaldo.silva7 <ronaldo.silva7@12SPLFS637718.seap.intranet>
2018-06-24 01:46:17 -0300

    IMPLEMENTADO CLASSE DE PRINCIPAL DA TELA PRÉ-LOCAÇÃO

---
+++ src/gestor/Modelo/PreLocacao.java
@@ -0,0 +1,183 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestor.Modelo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author ronal
+ */
+public class PreLocacao {
+
+    private int codigoReg;
+    private String statusReg;
+    private Date dataReg;
+    private String observacao;
+    private String usuarioInsert;
+    private String usuarioUp;
+    private String dataInsert;
+    private String dataUp;
+    private String horarioInsert;
+    private String horarioUp;
+
+    public PreLocacao() {
+    }
+
+    public PreLocacao(int codigoReg, String statusReg, Date dataReg, String observacao, String usuarioInsert, String usuarioUp, String dataInsert, String dataUp, String horarioInsert, String horarioUp) {
+        this.codigoReg = codigoReg;
+        this.statusReg = statusReg;
+        this.dataReg = dataReg;
+        this.observacao = observacao;
+        this.usuarioInsert = usuarioInsert;
+        this.usuarioUp = usuarioUp;
+        this.dataInsert = dataInsert;
+        this.dataUp = dataUp;
+        this.horarioInsert = horarioInsert;
+        this.horarioUp = horarioUp;
+    }
+
+    /**
+     * @return the codigoReg
+     */
+    public int getCodigoReg() {
+        return codigoReg;
+    }
+
+    /**
+     * @param codigoReg the codigoReg to set
+     */
+    public void setCodigoReg(int codigoReg) {
+        this.codigoReg = codigoReg;
+    }
+
+    /**
+     * @return the statusReg
+     */
+    public String getStatusReg() {
+        return statusReg;
+    }
+
+    /**
+     * @param statusReg the statusReg to set
+     */
+    public void setStatusReg(String statusReg) {
+        this.statusReg = statusReg;
+    }
+
+    /**
+     * @return the dataReg
+     */
+    public Date getDataReg() {
+        return dataReg;
+    }
+
+    /**
+     * @param dataReg the dataReg to set
+     */
+    public void setDataReg(Date dataReg) {
+        this.dataReg = dataReg;
+    }
+
+    /**
+     * @return the observacao
+     */
+    public String getObservacao() {
+        return observacao;
+    }
+
+    /**
+     * @param observacao the observacao to set
+     */
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    /**
+     * @return the usuarioInsert
+     */
+    public String getUsuarioInsert() {
+        return usuarioInsert;
+    }
+
+    /**
+     * @param usuarioInsert the usuarioInsert to set
+     */
+    public void setUsuarioInsert(String usuarioInsert) {
+        this.usuarioInsert = usuarioInsert;
+    }
+
+    /**
+     * @return the usuarioUp
+     */
+    public String getUsuarioUp() {
+        return usuarioUp;
+    }
+
+    /**
+     * @param usuarioUp the usuarioUp to set
+     */
+    public void setUsuarioUp(String usuarioUp) {
+        this.usuarioUp = usuarioUp;
+    }
+
+    /**
+     * @return the dataInsert
+     */
+    public String getDataInsert() {
+        return dataInsert;
+    }
+
+    /**
+     * @param dataInsert the dataInsert to set
+     */
+    public void setDataInsert(String dataInsert) {
+        this.dataInsert = dataInsert;
+    }
+
+    /**
+     * @return the dataUp
+     */
+    public String getDataUp() {
+        return dataUp;
+    }
+
+    /**
+     * @param dataUp the dataUp to set
+     */
+    public void setDataUp(String dataUp) {
+        this.dataUp = dataUp;
+    }
+
+    /**
+     * @return the horarioInsert
+     */
+    public String getHorarioInsert() {
+        return horarioInsert;
+    }
+
+    /**
+     * @param horarioInsert the horarioInsert to set
+     */
+    public void setHorarioInsert(String horarioInsert) {
+        this.horarioInsert = horarioInsert;
+    }
+
+    /**
+     * @return the horarioUp
+     */
+    public String getHorarioUp() {
+        return horarioUp;
+    }
+
+    /**
+     * @param horarioUp the horarioUp to set
+     */
+    public void setHorarioUp(String horarioUp) {
+        this.horarioUp = horarioUp;
+    }
+    
+}
