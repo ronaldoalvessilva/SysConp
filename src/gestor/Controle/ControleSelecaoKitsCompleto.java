befa389ef2af7e4cb9fe6df2f935b81e80702a8a
ronaldo.silva7 <ronaldo.silva7@12SPLFS637718.seap.intranet>
2018-07-31 21:36:46 -0300

    PERSISTENCIA PARA INTERNOS COM KIT COMPLETO.

---
+++ src/gestor/Controle/ControleSelecaoKitsCompleto.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestor.Controle;
+
+import gestor.Dao.ConexaoBancoDados;
+import gestor.Modelo.GravarInternosKitCompleto;
+import gestor.Modelo.ItensRequisicaoMateriaisInternos;
+import static gestor.Visao.TelaMontagemPagamentoKitInterno.qtdInternos;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ronal
+ */
+public class ControleSelecaoKitsCompleto {
+
+    ConexaoBancoDados conecta = new ConexaoBancoDados();
+    GravarInternosKitCompleto objGravaIntComp = new GravarInternosKitCompleto();
+
+    int codInterno;
+
+    public GravarInternosKitCompleto incluirInternosKitCompleto(GravarInternosKitCompleto objGravaIntComp) {
+        buscarInterno(objGravaIntComp.getNomeInternoCrc(), objGravaIntComp.getIdInternoCrc());
+        conecta.abrirConexao();
+        try {
+            PreparedStatement pst = conecta.con.prepareStatement("INSERT INTO ITENS_INTERNOS_AGRUPADOS_KIT_COMPLETO (IdRegistroComp,IdInternoCrc,Gravado,UsuarioInsert,DataInsert,HorarioInsert) VALUES(?,?,?,?,?,?)");
+            pst.setInt(1, objGravaIntComp.getIdRegistroComp());            
+            pst.setInt(2, codInterno);            
+            pst.setInt(3, objGravaIntComp.getGravado());
+            pst.setString(4, objGravaIntComp.getUsuarioInsert());
+            pst.setString(5, objGravaIntComp.getDataInsert());
+            pst.setString(6, objGravaIntComp.getHorarioInsert());
+            pst.execute();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Não Foi possivel INSERIR internos com kit completo.\nERRO: " + ex);
+        }
+        conecta.desconecta();
+        return objGravaIntComp;
+    }
+
+    public GravarInternosKitCompleto excluirInternosKitCompleto(GravarInternosKitCompleto objGravaIntComp) {
+
+        conecta.abrirConexao();
+        try {
+            PreparedStatement pst = conecta.con.prepareStatement("DELETE FROM ITENS_INTERNOS_AGRUPADOS_KIT_COMPLETO WHERE IdRegistroComp='" + objGravaIntComp.getIdRegistroComp() + "' AND IdInternoCrc='" + objGravaIntComp.getIdInternoCrc() + "'");
+            pst.executeUpdate();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Não Foi possivel EXCLUIR internos com kit completo.\nERRO: " + ex);
+        }
+        conecta.desconecta();
+        return objGravaIntComp;
+    }
+
+    public GravarInternosKitCompleto excluirTodosInternosKitCompleto(GravarInternosKitCompleto objGravaIntComp) {
+
+        conecta.abrirConexao();
+        try {
+            PreparedStatement pst = conecta.con.prepareStatement("DELETE FROM ITENS_INTERNOS_AGRUPADOS_KIT_COMPLETO WHERE IdRegistroComp='" + objGravaIntComp.getIdRegistroComp() + "'");
+            pst.executeUpdate();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Não Foi possivel EXCLUIR internos com kit completo.\nERRO: " + ex);
+        }
+        conecta.desconecta();
+        return objGravaIntComp;
+    }
+
+    public List<GravarInternosKitCompleto> read() throws Exception {
+        conecta.abrirConexao();
+        List<GravarInternosKitCompleto> listaInternosKitComp = new ArrayList<GravarInternosKitCompleto>();
+        try {
+            conecta.executaSQL("SELECT * FROM PRONTUARIOSCRC "
+                    + "INNER JOIN ITENS_INTERNOS_AGRUPADOS_KIT_COMPLETO "
+                    + "ON PRONTUARIOSCRC.IdInternoCrc=ITENS_INTERNOS_AGRUPADOS_KIT_COMPLETO.IdInternoCrc "
+                    + "ORDER BY PRONTUARIOSCRC.NomeInternoCrc");
+            while (conecta.rs.next()) {
+                GravarInternosKitCompleto pDigi = new GravarInternosKitCompleto();
+                pDigi.setIdInternoCrc(conecta.rs.getInt("IdInternoCrc"));
+                pDigi.setNomeInternoCrc(conecta.rs.getString("NomeInternoCrc"));
+                listaInternosKitComp.add(pDigi);
+                qtdInternos = qtdInternos + 1;
+            }
+            return listaInternosKitComp;
+        } catch (SQLException ex) {
+            Logger.getLogger(ControlePavilhaoInternosMontaKit.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            conecta.desconecta();
+        }
+        return null;
+    }
+
+    public void buscarInterno(String nome, int codigo) {
+        conecta.abrirConexao();
+        try {
+            conecta.executaSQL("SELECT * FROM PRONTUARIOSCRC WHERE NomeInternoCrc='" + nome + "' AND IdInternoCrc='" + codigo + "'");
+            conecta.rs.first();
+            codInterno = conecta.rs.getInt("IdInternoCrc");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Não Existe dados do PRODUTO a ser exibido !!!");
+        }
+        conecta.desconecta();
+    }
+}
