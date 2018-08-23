9a282206559439ea69907911015c59a68956dee7
ronaldo.silva7 <ronaldo.silva7@12SPLFS637718.seap.intranet>
2018-08-23 10:38:40 -0300

    IMPLEMENTADO MELHORIA DO CONTROLE DE ACESSO.

--- src/gestor/Visao/TelaEntradaSaidaInternosPortaria.java
+++ src/gestor/Visao/TelaEntradaSaidaInternosPortaria.java
@@ -1164,6 +1164,7 @@ public class TelaEntradaSaidaInternosPortaria extends javax.swing.JInternalFrame
 
     private void jBtNovoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtNovoActionPerformed
         // TODO add your handling code here:
+        buscarAcessoUsuario(telaEntradaSaidaLABManuP1);
         if (nameUser.equals("ADMINISTRADOR DO SISTEMA") || nomeGrupoP1.equals("ADMINISTRADORES") || codigoUserP1 == codUserAcessoP1 && nomeTelaP1.equals(telaEntradaSaidaLABManuP1) && codIncluirP1 == 1) {
             acao = 1;
             Novo();
@@ -1178,6 +1179,7 @@ public class TelaEntradaSaidaInternosPortaria extends javax.swing.JInternalFrame
 
     private void jBtAlterarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtAlterarActionPerformed
         // TODO add your handling code here:
+        buscarAcessoUsuario(telaEntradaSaidaLABManuP1);
         if (nameUser.equals("ADMINISTRADOR DO SISTEMA") || nomeGrupoP1.equals("ADMINISTRADORES") || codigoUserP1 == codUserAcessoP1 && nomeTelaP1.equals(telaEntradaSaidaLABManuP1) && codAlterarP1 == 1) {
             objEntSaiLabor.setStatusLanc(jStatusEntCola.getText());
             if (jStatusEntCola.getText().equals("FINALIZADO")) {
@@ -1197,6 +1199,7 @@ public class TelaEntradaSaidaInternosPortaria extends javax.swing.JInternalFrame
 
     private void jBtExcluirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtExcluirActionPerformed
         // TODO add your handling code here:
+        buscarAcessoUsuario(telaEntradaSaidaLABManuP1);
         if (nameUser.equals("ADMINISTRADOR DO SISTEMA") || nomeGrupoP1.equals("ADMINISTRADORES") || codigoUserP1 == codUserAcessoP1 && nomeTelaP1.equals(telaEntradaSaidaLABManuP1) && codExcluirP1 == 1) {
             objEntSaiLabor.setStatusLanc(jStatusEntCola.getText());
             if (jStatusEntCola.getText().equals("FINALIZADO")) {
@@ -1211,6 +1214,7 @@ public class TelaEntradaSaidaInternosPortaria extends javax.swing.JInternalFrame
 
     private void jBtSalvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtSalvarActionPerformed
         // TODO add your handling code here:
+        buscarAcessoUsuario(telaEntradaSaidaLABManuP1);
         if (nameUser.equals("ADMINISTRADOR DO SISTEMA") || nomeGrupoP1.equals("ADMINISTRADORES") || codigoUserP1 == codUserAcessoP1 && nomeTelaP1.equals(telaEntradaSaidaLABManuP1) && codGravarP1 == 1) {
             if (jDatalancamento.getDate() == null) {
                 JOptionPane.showMessageDialog(rootPane, "Informe a data do lancamento.");
