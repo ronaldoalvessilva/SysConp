734140a5d82b5683f7d723aff501585bff48b415
ronaldo.silva7 <ronaldo.silva7@12SPLFS637718.seap.intranet>
2018-08-23 09:21:45 -0300

    MELHORADO A PESQUISA DOS ACESSOS AOS BOTÕES INCLUIR, ALTERAR, EXCLUR E GRAVAR.

--- src/gestor/Visao/TelaEntradaSaidaOficialJusticaExterna.java
+++ src/gestor/Visao/TelaEntradaSaidaOficialJusticaExterna.java
@@ -1144,6 +1144,7 @@ public class TelaEntradaSaidaOficialJusticaExterna extends javax.swing.JInternal
 
     private void jBtNovoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtNovoActionPerformed
         // TODO add your handling code here:
+        buscarAcessoUsuario(telaEntradaSaidaOJManuP1E);
         if (nameUser.equals("ADMINISTRADOR DO SISTEMA") || nomeGrupoP1E.equals("ADMINISTRADORES") || codigoUserP1E == codUserAcessoP1E && nomeTelaP1E.equals(telaEntradaSaidaOJManuP1E) && codIncluirP1E == 1) {
             acao = 1;
             Novo();
@@ -1158,6 +1159,7 @@ public class TelaEntradaSaidaOficialJusticaExterna extends javax.swing.JInternal
 
     private void jBtAlterarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtAlterarActionPerformed
         // TODO add your handling code here:
+        buscarAcessoUsuario(telaEntradaSaidaOJManuP1E);
         if (nameUser.equals("ADMINISTRADOR DO SISTEMA") || nomeGrupoP1E.equals("ADMINISTRADORES") || codigoUserP1E == codUserAcessoP1E && nomeTelaP1E.equals(telaEntradaSaidaOJManuP1E) && codAlterarP1E == 1) {
             objEntSaiOficialJustica.setStatusLanc(jStatusEntCola.getText());
             if (jStatusEntCola.getText().equals("FINALIZADO")) {
@@ -1177,6 +1179,7 @@ public class TelaEntradaSaidaOficialJusticaExterna extends javax.swing.JInternal
 
     private void jBtExcluirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtExcluirActionPerformed
         // TODO add your handling code here:
+        buscarAcessoUsuario(telaEntradaSaidaOJManuP1E);
         if (nameUser.equals("ADMINISTRADOR DO SISTEMA") || nomeGrupoP1E.equals("ADMINISTRADORES") || codigoUserP1E == codUserAcessoP1E && nomeTelaP1E.equals(telaEntradaSaidaOJManuP1E) && codExcluirP1E == 1) {
             objEntSaiOficialJustica.setStatusLanc(jStatusEntCola.getText());
             if (jStatusEntCola.getText().equals("FINALIZADO")) {
@@ -1191,6 +1194,7 @@ public class TelaEntradaSaidaOficialJusticaExterna extends javax.swing.JInternal
 
     private void jBtSalvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtSalvarActionPerformed
         // TODO add your handling code here:
+        buscarAcessoUsuario(telaEntradaSaidaOJManuP1E);
         if (nameUser.equals("ADMINISTRADOR DO SISTEMA") || nomeGrupoP1E.equals("ADMINISTRADORES") || codigoUserP1E == codUserAcessoP1E && nomeTelaP1E.equals(telaEntradaSaidaOJManuP1E) && codGravarP1E == 1) {
             if (jDatalancamento.getDate() == null) {
                 JOptionPane.showMessageDialog(rootPane, "Informe a data do lancamento.");
