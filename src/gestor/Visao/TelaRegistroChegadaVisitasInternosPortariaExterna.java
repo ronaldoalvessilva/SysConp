ec2c251d916ff8eb2df7bebfabb8e26d862bda77
ronaldo.silva7 <ronaldo.silva7@12SPLFS637718.seap.intranet>
2018-09-10 14:30:18 -0300

    CORRIGIDO O CONTROLE DE ACESSO NOS BOTÕES

--- src/gestor/Visao/TelaRegistroChegadaVisitasInternosPortariaExterna.java
+++ src/gestor/Visao/TelaRegistroChegadaVisitasInternosPortariaExterna.java
@@ -25,9 +25,14 @@ import gestor.Modelo.VisitaInterno;
 import static gestor.Visao.TelaLoginSenha.descricaoUnidade;
 import static gestor.Visao.TelaLoginSenha.nameUser;
 import static gestor.Visao.TelaModuloPortariaExterna.codAbrirP1E;
+import static gestor.Visao.TelaModuloPortariaExterna.codAlterarP1E;
+import static gestor.Visao.TelaModuloPortariaExterna.codConsultarP1E;
+import static gestor.Visao.TelaModuloPortariaExterna.codExcluirP1E;
 import static gestor.Visao.TelaModuloPortariaExterna.codGravarP1E;
 import static gestor.Visao.TelaModuloPortariaExterna.codIncluirP1E;
 import static gestor.Visao.TelaModuloPortariaExterna.codUserAcessoP1E;
+import static gestor.Visao.TelaModuloPortariaExterna.codigoGrupoP1E;
+import static gestor.Visao.TelaModuloPortariaExterna.codigoUserGroupP1E;
 import static gestor.Visao.TelaModuloPortariaExterna.codigoUserP1E;
 import static gestor.Visao.TelaModuloPortariaExterna.nomeGrupoP1E;
 import static gestor.Visao.TelaModuloPortariaExterna.nomeTelaP1E;
@@ -1047,6 +1052,7 @@ public class TelaRegistroChegadaVisitasInternosPortariaExterna extends javax.swi
 
     private void jBtIniciarLeitorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtIniciarLeitorActionPerformed
         // TODO add your handling code here:    
+        buscarAcessoUsuario(telaRegistroChegadaPortExtBioP1E);
         if (nameUser.equals("ADMINISTRADOR DO SISTEMA") || nomeGrupoP1E.equals("ADMINISTRADORES") || codigoUserP1E == codUserAcessoP1E && nomeTelaP1E.equals(telaRegistroChegadaPortExtBioP1E) && codAbrirP1E == 1) {
             // Instanciar a DLL
             CIS_SDK dll = CIS_SDK.INSTANCE;
@@ -1132,6 +1138,7 @@ public class TelaRegistroChegadaVisitasInternosPortariaExterna extends javax.swi
 
     private void jBtSalvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtSalvarActionPerformed
         // TODO add your handling code here:
+        buscarAcessoUsuario(telaRegistroChegadaPortExtManuP1E);
         if (nameUser.equals("ADMINISTRADOR DO SISTEMA") || nomeGrupoP1E.equals("ADMINISTRADORES") || codigoUserP1E == codUserAcessoP1E && nomeTelaP1E.equals(telaRegistroChegadaPortExtManuP1E) && codGravarP1E == 1) {
             jStatus.setText("FINALIZADO");
 //            verificarOrdemChegada();
@@ -1222,6 +1229,7 @@ public class TelaRegistroChegadaVisitasInternosPortariaExterna extends javax.swi
 
     private void jBtNovoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtNovoActionPerformed
         // TODO add your handling code here:
+        buscarAcessoUsuario(telaRegistroChegadaPortExtManuP1E);
         if (nameUser.equals("ADMINISTRADOR DO SISTEMA") || nomeGrupoP1E.equals("ADMINISTRADORES") || codigoUserP1E == codUserAcessoP1E && nomeTelaP1E.equals(telaRegistroChegadaPortExtManuP1E) && codIncluirP1E == 1) {
             Novo();
             corCampos();
@@ -2119,5 +2127,43 @@ public class TelaRegistroChegadaVisitasInternosPortariaExterna extends javax.swi
         objLogSys.setIdLancMov(Integer.valueOf(jRegistro.getText()));
         objLogSys.setNomeUsuarioLogado(nameUser);
         objLogSys.setStatusMov(statusMov);
+    }
+
+    public void buscarAcessoUsuario(String nomeTelaAcesso) {
+        conecta.abrirConexao();
+        try {
+            conecta.executaSQL("SELECT * FROM USUARIOS "
+                    + "WHERE NomeUsuario='" + nameUser + "'");
+            conecta.rs.first();
+            codigoUserP1E = conecta.rs.getInt("IdUsuario");
+        } catch (Exception e) {
+        }
+        try {
+            conecta.executaSQL("SELECT * FROM USUARIOS_GRUPOS "
+                    + "INNER JOIN GRUPOUSUARIOS "
+                    + "ON USUARIOS_GRUPOS.IdGrupo=GRUPOUSUARIOS.IdGrupo "
+                    + "WHERE IdUsuario='" + codigoUserP1E + "'");
+            conecta.rs.first();
+            codigoUserGroupP1E = conecta.rs.getInt("IdUsuario");
+            codigoGrupoP1E = conecta.rs.getInt("IdGrupo");
+            nomeGrupoP1E = conecta.rs.getString("NomeGrupo");
+        } catch (Exception e) {
+        }
+        try {
+            conecta.executaSQL("SELECT * FROM TELAS_ACESSO "
+                    + "WHERE IdUsuario='" + codigoUserP1E + "' "
+                    + "AND NomeTela='" + nomeTelaAcesso + "'");
+            conecta.rs.first();
+            codUserAcessoP1E = conecta.rs.getInt("IdUsuario");
+            codAbrirP1E = conecta.rs.getInt("Abrir");
+            codIncluirP1E = conecta.rs.getInt("Incluir");
+            codAlterarP1E = conecta.rs.getInt("Alterar");
+            codExcluirP1E = conecta.rs.getInt("Excluir");
+            codGravarP1E = conecta.rs.getInt("Gravar");
+            codConsultarP1E = conecta.rs.getInt("Consultar");
+            nomeTelaP1E = conecta.rs.getString("NomeTela");
+        } catch (Exception e) {
+        }
+        conecta.desconecta();
     }
 }
