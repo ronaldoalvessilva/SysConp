75513f9ba471ab962a4c4d4aa387a2f6ca0c435d
ronaldo.silva7 <ronaldo.silva7@12SPLFS637718.seap.intranet>
2018-09-17 14:09:50 -0300

    CORRIGIDO ERRO NO NOME DO CAMPOS, ESTAVA DATALANC E O CORREOT É DATAREG.

--- src/gestor/Visao/TelaRegistroInternosAtendimentoTO.java
+++ src/gestor/Visao/TelaRegistroInternosAtendimentoTO.java
@@ -889,7 +889,7 @@ public class TelaRegistroInternosAtendimentoTO extends javax.swing.JInternalFram
                     preencherTabelaRegistros("SELECT * FROM REGISTRO_ATENDIMENTO_INTERNO_PSP "
                             + "INNER JOIN PRONTUARIOSCRC "
                             + "ON REGISTRO_ATENDIMENTO_INTERNO_PSP.IdInternoCrc=PRONTUARIOSCRC.IdInternoCrc "
-                            + "WHERE DataLanc BETWEEN'" + dataInicial + "' "
+                            + "WHERE DataReg BETWEEN'" + dataInicial + "' "
                             + "AND '" + dataFinal + "' "
                             + "AND IdDepartamento='" + codigoDepto + "'");
                 }
