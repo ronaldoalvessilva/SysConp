4f0cc7091dc9bcb6b4d2d129e5bdbf1ffbab2e57
ronaldo.silva7 <ronaldo.silva7@12SPLFS637718.seap.intranet>
2018-09-26 09:13:08 -0300

    MODIFICADO A PESQUISA DO NOME DO PAVILHÃO, TIRADO A CONDIÇÃO DE IGUAL E COLOCADO LIKE.

--- src/gestor/Visao/TelaAlertaBasesPavilhoes.java
+++ src/gestor/Visao/TelaAlertaBasesPavilhoes.java
@@ -1521,8 +1521,8 @@ public class TelaAlertaBasesPavilhoes extends javax.swing.JInternalFrame {
         conecta.abrirConexao();
         try {
             conecta.executaSQL("SELECT * FROM PAVILHAO "
-                    + "WHERE DescricaoPav='" + descricao + "' "
-                    + "OR DescricaoPav='" + descricao2 + "'");
+                    + "WHERE DescricaoPav LIKE'%" + descricao + "%' "
+                    + "OR DescricaoPav LIKE'%" + descricao2 + "%'");
             conecta.rs.first();
             codigoPavilhao = conecta.rs.getInt("IdPav");
             descricaoPavilhao = conecta.rs.getString("DescricaoPav");
