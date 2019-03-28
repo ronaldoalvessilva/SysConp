28a8fc8299914ec4629ac902693b7eb5395d7644
ronaldo.silva7 <ronaldo.silva7@12SPLFS637718.seap.intranet>
2019-03-28 13:09:39 -0300

    RETIRADO AS LINHAS DAS TABELAS.

--- src/gestor/Visao/TelaEntradaProdutosAC.java
+++ src/gestor/Visao/TelaEntradaProdutosAC.java
@@ -518,7 +518,7 @@ public class TelaEntradaProdutosAC extends javax.swing.JInternalFrame {
         jTabelaNFeCompras.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED));
         jTabelaNFeCompras.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null, null, null}
+
             },
             new String [] {
                 "Código", "Dt.Emissão", "Status NFE", "Dt.Entrada", "Fornecedor", "Data Val."
@@ -1501,7 +1501,7 @@ public class TelaEntradaProdutosAC extends javax.swing.JInternalFrame {
         jTabelaItensNFeCompras.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED));
         jTabelaItensNFeCompras.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null, null, null, null}
+
             },
             new String [] {
                 "Item", "Código", "Descrição Produto", "Qtd.", "Valor UN", "Data Val.", "Lote"
@@ -1786,7 +1786,7 @@ public class TelaEntradaProdutosAC extends javax.swing.JInternalFrame {
         jTabelaParcelas.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED));
         jTabelaParcelas.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null}
+
             },
             new String [] {
                 "Código", "Vencimento", "Valor Parcela", "Valor Doc."
