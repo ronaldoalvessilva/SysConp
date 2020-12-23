7ce5d876b386dd81e6926eadf7b7854b80bb03dc
ronaldo.silva7 <ronaldo.silva7@12SPLFS637718.seap.intranet>
2020-12-23 15:51:19 -0200

    - MELHORADO O CAMPO GRAU DE PARENTESCO DA VISITA COM O INTERNO, RETIRANDO O AMPO ABERTO E DEIXANDO A SELEÇÃO DOS PARENTESCO. - FOI APLICADO TAMBÉM NA MONTAGEM DO ROL DE VISITAS, PARA O CASO DE UMA VISITA TER MAIS DE UM INTERNO SER VISITADO, COMO EXEMPLO, A VISITA IRÁ VISITA O COMPANHEIRO DELA E O FILHO. COM ISSO, AGORA É POSSÍVEL ESCOLHER O GRAU DE PARENTESCO DO VISITANTE NO MOMENTO DA MONTAGEM DO ROL.

---
+++ SCRIPT_MODIFICAR_GRAU_PARENTESCO_VISITAS_INTERNOS.sql
@@ -0,0 +1,23 @@
+/*
+Filho(a)
+Companheiro(a)
+Mãe
+Irmão(ã)
+Pai
+Avô(ó)
+Padastro
+Madrasta
+Primo(a)
+Genro
+Nora
+Tio(a)
+Sobrinho(a)
+Esposo(a)
+Madastra
+Padastro
+Cunhado(a)
+Enteado(a)
+*/
+SELECT * FROM VISITASINTERNO
+UPDATE VISITASINTERNO SET ParentescoVisita='Companheiro(a)' WHERE ParentescoVisita='COMPAMHEIRA' OR ParentescoVisita='EVANGELICA' OR ParentescoVisita='COMP'
+
--- src/gestor/Visao/TelaVisitaSocial.java
+++ src/gestor/Visao/TelaVisitaSocial.java
@@ -388,6 +388,7 @@ public class TelaVisitaSocial extends javax.swing.JInternalFrame {
                 .addContainerGap())
         );
 
+        jTabelaVisitas.setAutoCreateRowSorter(true);
         jTabelaVisitas.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED));
         jTabelaVisitas.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -731,7 +732,7 @@ public class TelaVisitaSocial extends javax.swing.JInternalFrame {
         });
 
         jComboBoxParentesco.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        jComboBoxParentesco.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Selecione...", "Companheiro(a)", "Esposo(a)", "Irmão(ã)", "Tio(a)", "Sobrinho(a)", "Avô(ó)", "Pai", "Mãe", "Filho(a)", "Mãe Adotiva", "Pai Adotivo", "Padastro", "Madastra", "Cunhado(a)", "Primo(a)", "Genro", "Nora", "Neto(a)", "Outros", " " }));
+        jComboBoxParentesco.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Selecione...", "Companheiro(a)", "Esposo(a)", "Irmão(ã)", "Tio(a)", "Sobrinho(a)", "Avô(ó)", "Pai", "Mãe", "Filho(a)", "Filho(a) Adotivo", "Mãe Adotiva", "Pai Adotivo", "Padastro", "Madastra", "Cunhado(a)", "Primo(a)", "Genro", "Nora", "Neto(a)", "Enteado(a)", "Bisneto(a)", "Namorado(a)", "Outros", " " }));
         jComboBoxParentesco.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED));
         jComboBoxParentesco.setEnabled(false);
 
